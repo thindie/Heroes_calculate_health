8c2e972a4be0f758dbe590591441642c0226d31b
Alex <thindie1@mail.ru>
2022-12-24 23:45:50 +0200

    addition 3 (compose)

---
+++ app/src/main/java/com/example/thindie/heroes/presentation/HeroesViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.thindie.heroes.presentation
+
+import androidx.lifecycle.ViewModel
+
+class HeroesViewModel : ViewModel() {
+}
--- app/src/main/java/com/example/thindie/heroes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/thindie/heroes/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.thindie.heroes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.lifecycle.ViewModelProvider
 import com.example.thindie.heroes.R
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: HeroesViewModel by lazy {
+        ViewModelProvider(this)[HeroesViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent { 
+            Surface(
+                content = { Heroes(viewModel = viewModel) }
+            )
+        }
     }
 }
